3d04c94304b036e0e1cec5aee1993831d8453898
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-10 14:43:57 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -21,8 +21,16 @@ def call
    case gets.chomp
    when 'list songs'
      self.list_songs 
-   end
-    
+   when 'list artists'
+     self.list_artists
+     when 'list genres'
+      self.list_genres
+      when 'list artist'
+        self.list_songs_by_artist
+        when 'list genre'
+          self.list_songs_by_genre
+          when 'play song'
+            self.play_song
   end
 end
 
