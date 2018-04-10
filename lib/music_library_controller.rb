ba693dd0b9dedd01267a9519177d62b7fc5b9c22
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-10 14:50:16 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -18,7 +18,7 @@ def call
    puts "To play a song, enter 'play song'."
    puts "To quit, type 'exit'."
     puts "What would you like to do?"
-   unless input == "exit"
+   unless gets.chomp == "exit"
     puts "Welcome to your music library!"
   puts "To list all of your songs, enter 'list songs'."
   puts "To list all of the artists in your library, enter 'list artists'."
@@ -29,7 +29,7 @@ def call
    puts "To quit, type 'exit'."
     puts "What would you like to do?"
    
-   case input
+   case gets.chomp
    when 'list songs'
      self.list_songs 
    when 'list artists'
