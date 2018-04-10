18ed971973274adb0d3cace42c19cd1ff06e4bee
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-04-10 14:50:42 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -19,7 +19,6 @@ def call
    puts "To quit, type 'exit'."
     puts "What would you like to do?"
    unless gets.chomp == "exit"
-    puts "Welcome to your music library!"
   puts "To list all of your songs, enter 'list songs'."
   puts "To list all of the artists in your library, enter 'list artists'."
   puts "To list all of the genres in your library, enter 'list genres'."
